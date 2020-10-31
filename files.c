58608a29d3492c4f9abb2393a63ab694155df74d
sana1209 <72398751+sana1209@users.noreply.github.com>
2020-10-31 11:34:00 +0530

    Create files.c

---
+++ files.c
@@ -0,0 +1,17 @@
+include<stdio.h>
+#include<conio.h>
+main()
+{
+	//1. Declare the FILE pointer
+	
+	//2. OPEN - Read, Write and Append
+	
+	//3. READ content from the file
+	
+	//4. WRITE content to the file 
+	
+	//5. APPEND content to the file 
+	
+	//6. CLOSE the pointer
+	
+  }
